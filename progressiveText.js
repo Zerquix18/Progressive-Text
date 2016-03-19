dc2c250f3582e0b8e8bd016318de3a32c01a7d0f
I'm Luis! ^-^ <malbertoa_11@hotmail.com>
2016-03-18 22:22:18 -0400

    first commit

---
+++ progressiveText.js
@@ -0,0 +1,41 @@
+/**
+*
+* Shows multiples text
+* progressively
+*
+* @author Zerquix18
+*
+**/
+window.progressiveText = {
+
+	totalProgressives: [],
+
+	start: function( selector, texts, interval ) {
+		
+		interval = interval || 5;
+		this.totalProgressives[ selector ] = [];
+		var miliSeconds,
+			changeText = function( selector, text ) {
+				$(selector).text(text);
+			};
+
+		for( var i = 0; i < texts.length; i++ ) {
+			miliSeconds = interval * (i * 1000);
+			var id = window.setTimeout(
+				changeText,
+				miliSeconds,
+				selector,
+				texts[i]
+			);
+			this.totalProgressives[ selector ].push( id );
+		}
+	},
+
+	stop: function( selector ) {
+
+		for( var i = 0; i < this.totalProgressives[ selector ].length; i++)
+			window.clearTimeout( this.totalProgressives[ selector ][i] );
+
+	}
+
+};
